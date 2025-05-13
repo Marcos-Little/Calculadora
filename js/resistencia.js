bb0d09be2a62bf40c9ebc31edd6672bc583fd660
Marcos Oliveira <aoliveirajunio0@gmail.com>
2025-05-13 18:59:33 -0300

    Add files via upload

---
+++ js/resistencia.js
@@ -0,0 +1,20 @@
+function resistencia1 (){
+    let potencia1 = parseFloat(document.getElementById("potencia1").value)
+    let corrente1 = parseFloat(document.getElementById("potencia1").value)
+    let resultado = (potencia1 / (corrente1 * corrente1)).toFixed(2)
+    alert("O resultado da resistência é " + resultado)
+}
+
+function resistencia2 (){
+    let tensao2 = parseFloat(document.getElementById("tensao2").value)
+    let potencia2 = parseFloat(document.getElementById("tensao2").value)
+    let resultado = ((tensao2 * tensao2) / potencia2).toFixed(2)
+    alert("O resultado da resistência é " + resultado)
+}
+
+function resistencia3() {
+    let tensao3 = parseFloat(document.getElementById("tensao3").value)
+    let potencia3 = parseFloat(document.getElementById("tensao3").value)
+    let resultado = (tensao3 / potencia3).toFixed(2)
+    alert("O resultado da resistência é " + resultado)
+}
